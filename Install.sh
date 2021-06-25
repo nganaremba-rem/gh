405c367f9c85790ad1c8d960a01786ddc0133ddf
remku <83505774+remku@users.noreply.github.com>
2021-06-25 16:12:19 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,4 +1,6 @@
+[[ "$OSTYPE" == "linux-gnu" ]] && { device="Linux"; } || { [[ "$OSTYPE" == "linux-android" ]] && { device="Android"}; }
+[[ "$device" == "Linux" ]] && { yes | sudo apt install curl; cmd="sudo apt" } || { yes | pkg install curl; cmd="pkg" }
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+$cmd update
+$cmd install gh
